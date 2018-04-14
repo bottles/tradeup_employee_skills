9692ac99be480285e16f4d846e9aac8049a77168
JenGao <jen.gao@instaclustr>
2018-04-14 21:58:09 +1000

    configure connection to mysql database

---
+++ initial.sql
@@ -0,0 +1,11 @@
+#CREATE DATABASE tradeup_employee_skill;
+
+USE tradeup_employee_skill;
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(1000),
+  PRIMARY KEY (id)
+);
+
+
